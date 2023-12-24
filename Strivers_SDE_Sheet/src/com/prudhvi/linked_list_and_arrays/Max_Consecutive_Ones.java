60d3938db84638a0ddaec7e6b955f111f7a561c3
Bavigadda <prudhviraj729@gmail.com>
2023-12-24 20:00:33 +0530

    485. Max Consecutive Ones

--- Strivers_SDE_Sheet/src/com/prudhvi/linked_list_and_arrays/Max_Consecutive_Ones.java
+++ Strivers_SDE_Sheet/src/com/prudhvi/linked_list_and_arrays/Max_Consecutive_Ones.java
@@ -8,6 +8,19 @@ public class Max_Consecutive_Ones {
 		System.out.println( maxConsecutiveOnes);
 
 	}
+	/*
+	This method finds the maximum consecutive ones streak in an array of 0s and 1s. 
+	It iterates through the array, updating the currentStreak based on the current element (1 or 0). 
+	Whenever a 0 is encountered, the current streak is reset to 0. 
+	The maximum streak is updated by taking the maximum of the current streak and the existing maximum. 
+	The final result is the maximum consecutive ones streak.
+	
+	Time Complexity:
+		The time complexity is O(N), where N is the length of the nums array. The algorithm iterates through the array once.
+
+	Space Complexity:
+		The space complexity is O(1) since the algorithm uses only a constant amount of extra space. The variables currentStreak and maxStreak are used to store intermediate results.
+	 */
 	public static int findMaxConsecutiveOnes(int[] nums) {
 		// Variable to track the current consecutive ones streak
 	    int currentStreak = 0;  
@@ -25,6 +38,4 @@ public class Max_Consecutive_Ones {
 	    // Return the maximum consecutive ones streak
 	    return maxStreak;  
 	}
-
-
 }
